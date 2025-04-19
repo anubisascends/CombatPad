5c9a0d6ddd0a478f64368fd673ac0f7da64e942d
anubisascends <anubisascends@gmail.com>
2025-04-19 12:30:20 -0400

    Got D&D Modifier ability scores and modifiers working

---
+++ CombatPad/Classes/AbilityScoreConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CombatPad.Classes
+{
+    public class AbilityScoreConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bValue = System.Convert.ToByte(value);
+
+            return RpgMath.Modifier(bValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
