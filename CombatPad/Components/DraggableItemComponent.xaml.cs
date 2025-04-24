71c96cfd45f8fe11a0350dfcd411ed7c8134b547
anubisascends <anubisascends@gmail.com>
2025-04-24 06:47:40 -0400

    Componentized several items, including the Initiatve panel to make this easier to work with

---
+++ CombatPad/Components/DraggableItemComponent.xaml.cs
@@ -0,0 +1,26 @@
+﻿using CombatPad.Models;
+using System.Windows;
+
+namespace CombatPad.Components
+{
+    /// <summary>
+    /// Interaction logic for DraggableItemComponent.xaml
+    /// </summary>
+    public partial class DraggableItemComponent
+    {
+        public DraggableItemComponent()
+        {
+            InitializeComponent();
+        }
+
+        public ListItem Item
+        {
+            get { return (ListItem)GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Item.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemProperty =
+            DependencyProperty.Register("Item", typeof(ListItem), typeof(DraggableItemComponent), new PropertyMetadata(null));
+    }
+}
--- CombatPad/Components/InitiativePanel.xaml.cs
+++ CombatPad/Components/InitiativePanel.xaml.cs
@@ -1,28 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CombatPad.Components
 {
     /// <summary>
     /// Interaction logic for InitiativePanel.xaml
     /// </summary>
-    public partial class InitiativePanel : UserControl
+    public partial class InitiativePanel
     {
         public InitiativePanel()
         {
             InitializeComponent();
         }
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public object SelectedItem
+        {
+            get { return (object)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedItem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(object), typeof(InitiativePanel), new PropertyMetadata(null));
+        
+        // Using a DependencyProperty as the backing store for ItemsSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(InitiativePanel), new PropertyMetadata(null));
     }
 }
